210604628c7906502ca8ae17224ebf0da403c955
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-17 20:51:33 +0530

    variables in java

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int age=19;
+        String name="sudarshan";
+        boolean male=true;//in java it boolean auther then bool
+        float per=83.33f;//always write f or F at the end
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(male);
+        System.out.println(per);
+    }
+}
