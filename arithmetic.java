bf78315e045caef17235d6d49af646e38b4b225b
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-17 21:54:06 +0530

    basic arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+//basic arithmetic operator * + \ -
+public class arithmetic {
+    public static void main(String[] args){
+        int x=50;
+        float y=30;
+        System.out.println(x+y);//use to add to number
+        System.out.println(x-y);//use to sub two number
+        System.out.println(x*y);//use to mult to number
+        System.out.println(x/y);//use to div two number
+//NOTE:in case of(/)it return int div for float div change "int y" to "float y"
+}
+    
+}
