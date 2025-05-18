7b4cdaa40907a34c4ae52f409be365f28fb317fb
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-18 19:06:30 +0530

    add char

--- variable.java
+++ variable.java
@@ -4,9 +4,11 @@ public class variable {
         String name="sudarshan";
         boolean male=true;//in java it boolean auther then bool
         float per=83.33f;//always write f or F at the end
+        char grade='O';
         System.out.println(age);
         System.out.println(name);
         System.out.println(male);
         System.out.println(per);
+        System.out.println(grade);
     }
 }
