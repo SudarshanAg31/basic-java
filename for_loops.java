c640d844c02f2b716a174564c9e7919414bbbca0
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-23 23:09:27 +0530

    basic for loop

---
+++ for_loops.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class for_loops {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int x=sc.nextInt();
+    //for positive loop
+        for(int i=0;i<=x;i++){
+            System.out.println("sudarshan");
+        }
+    // -ne loop
+        for(int i=10;i>=0;i--){
+            System.out.println(i);
+        }
+    }
+}
