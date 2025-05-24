827ec54d9c5f792bc21593f88fc7e6a4c85f3ceb
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-24 19:59:52 +0530

    learn about while loop

---
+++ while_loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class while_loop {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int i=1;
+        while(i<=x){
+            System.out.println(i);
+            i++;
+        }
+        sc.close();
+    }
+}
