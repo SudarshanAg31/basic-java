91ea58d6fac4b056b91bb6ea90e11294c00f9517
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-27 22:40:26 +0530

    do while

---
+++ do_while_loop.java
@@ -0,0 +1,11 @@
+
+public class do_while_loop {
+    public static void main(String[] args) {
+        //first it run then it checks condition
+        int i=1000;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=1);
+    }
+}
