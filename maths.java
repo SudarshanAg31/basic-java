670e2ce93b663034db726535988a506ca1bcd880
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-05-28 23:14:35 +0530

    maths functions

---
+++ maths.java
@@ -0,0 +1,18 @@
+//these are basic math inbuilt function
+public class maths {
+    public static void main(String[] args) {
+        int x=64;
+        int x1=65;
+        double y=-4.54;
+        System.out.println(Math.pow(3, 9));//it return double
+        System.out.println(Math.sqrt(x));//it return double
+        System.out.println(Math.cbrt(x));//it return double
+        System.out.println(Math.ceil(y));//it return double
+        System.out.println(Math.floor(y));//it return double
+        System.out.println(Math.max(x, x1));//it return in same datatype
+        System.out.println(Math.min(x, x1));//it return in same datatype
+        System.out.println(Math.round(y));//it return int
+        System.out.println(Math.abs(y));//it return double
+
+    }
+}
