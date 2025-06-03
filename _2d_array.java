383795b03fc4f90e14cf5c532ecd3b31d3c2666c
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-06-03 23:41:08 +0530

    taking 2d array input

--- _2d_array.java
+++ _2d_array.java
@@ -1,16 +1,32 @@
-
-
+import java.util.Scanner;
 public class _2d_array {
-    public static void main(String[] args) {
-        // Scanner sc= new Scanner(System.in);
-        // int x=sc.nextInt();
-        // int y=sc.nextInt();
-        int[][] x={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
-        for(int i=0;i<x.length;i++){
-            for(int j=0;j<x[i].length;j++){
-                System.out.print(x[i][j]+" ");
+    public static void printarr(int[][] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println("");
-        }        
+        }   
+    }
+    public static int[][] inputarr(int[][] arr,int x,int y) {
+        Scanner sc= new Scanner(System.in);
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        //first way without input
+        int[][] arr={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
+        printarr(arr);        
+        //second input array 
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int[][] arr1=new int[x][y];
+        int[][] arr2=inputarr(arr1, x, y);
+        printarr(arr2);
     }
 }
