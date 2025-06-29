73d57f43e636f3af84e77a16bcbda03258af97b9
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-06-30 01:40:17 +0530

    basic string

---
+++ string_basic.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class string_basic {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        // char []arr={'s','u','d','a','r','s','h','a','n'};//way to print string
+        // for(int i=0;i<arr.length;i++)
+        // System.out.print(arr[i]);
+
+        String str="sudarshan agrawal";//effective way to print string
+        //System.out.println(str);
+
+        // String str1=sc.next();//it can't bear whitespace
+        // System.out.println(str1);
+        // sc.nextLine();
+
+        // String str2=sc.nextLine();//it can bear whitespace
+        // System.out.println(str2);
+
+        // char ch=str.charAt(3);//it can return chat present at index 3
+        // System.out.println(ch);
+
+        // int idx=str.indexOf('a');//it can return first index of 'a'; 
+        // System.out.println(idx);
+
+        // String str1="sudarshan agrawal";
+        // System.out.println(str.compareTo(str1));
+
+        // System.out.print(str.concat(str));
+    }
+}
