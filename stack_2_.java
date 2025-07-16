fb59df6c9560a4f98e011c54e2a8ae436099152d
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-07-16 23:30:24 +0530

    Implement stack transfer functionality to move elements between stacks while maintaining order

---
+++ stack_2_.java
@@ -0,0 +1,27 @@
+//move from one stack to another
+import java.util.Stack;
+public class stack_2_ {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        Stack<Integer> st1=new Stack<>();
+        while(st.size()!=0){
+            int x=st.pop();
+            st1.push(x);
+        }
+        //first it transfer from st to st1 in reverse order
+        Stack<Integer> st2=new Stack<>();
+        while(st1.size()!=0){
+            int x=st1.pop();
+            st2.push(x);
+        }
+        System.out.println(st);//due to this we move data from one stack to another 
+        System.out.println(st2);//then it fransfer from st1 to st2 so, 
+        //now it is in same order  
+    }
+}
