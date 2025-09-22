3c1b4c81cad3fdc05b30c6723dfc8661536301e4
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-09-22 23:29:36 +0530

    Add Student class with parameterized constructor and main method

---
+++ oops_constructor2.java
@@ -0,0 +1,15 @@
+class Student{
+    int age;
+    String name;
+    Student(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+}
+public class oops_constructor2 {
+    public static void main(String[] args) {
+        Student s1=new Student(11, "sudarshan");
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+    }
+}
